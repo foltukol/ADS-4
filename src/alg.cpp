f7cf8f923aba33af78c5554d53fdc09f6613c4e9
foltukol <foltukol@yandex.ru>
2025-04-11 12:51:55 +0300

    ADS-4 | b51 | Kurmaev Andrey

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,58 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int pair_numbers = 0;
+  for (int i = 0; i < len; ++i) {
+    for (int j = i + 1; j < len; ++j) {
+      if (arr[i] + arr[j] == value) {
+        pair_numbers++;
+      }
+    }
+  } 
+  return pair_numbers;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int pair_numbers = 0;
+  int left_number = 0;
+  int right_number = len - 1;
+  while (left_number < right_number) {
+    int sum = arr[left_number] + arr[right_number];
+    if (sum == value) {
+      pair_numbers++;
+      left_number++;
+      right_number--;
+    } 
+    else if (sum < value) {
+      left_number++;
+    } 
+    else {
+    right_number--;
+    }
+  }
+  return pair_numbers;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int pair_numbers = 0;
+  for (int i = 0; i < len - 1; ++i) {
+    int target = value - arr[i];
+    int left = i + 1;
+    int right = len - 1;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (arr[mid] == target) {
+        pair_numbers++;
+        for (int j = mid - 1; j >= left && arr[j] == target; --j)
+          pair_numbers++;
+        for (int j = mid + 1; j <= right && arr[j] == target; ++j)
+          pair_numbers++;
+          break;
+      }
+      else if (arr[mid] < target) {
+        left = mid + 1;
+      }
+      else {
+        right = mid - 1;
+      }
+    }
+  }
+  return pair_numbers;
 }
