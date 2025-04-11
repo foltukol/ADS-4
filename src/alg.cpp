76c5aa3f2301db40ef1410b074c841b3afa9d4e2
foltukol <foltukol@yandex.ru>
2025-04-11 12:56:08 +0300

    ADS-4 | b51 | Kurmaev Andrey

--- src/alg.cpp
+++ src/alg.cpp
@@ -33,24 +33,24 @@ int countPairs2(int *arr, int len, int value) {
 int countPairs3(int *arr, int len, int value) {
   int pair_numbers = 0;
   for (int i = 0; i < len - 1; ++i) {
-    int target = value - arr[i];
-    int left = i + 1;
-    int right = len - 1;
-    while (left <= right) {
-      int mid = left + (right - left) / 2;
-      if (arr[mid] == target) {
+    int find_number = value - arr[i];
+    int left_number = i + 1;
+    int right_number = len - 1;
+    while (left_number <= right_number) {
+      int middle = left_number + (right_number - left_number) / 2;
+      if (arr[middle] == find_number) {
         pair_numbers++;
-        for (int j = mid - 1; j >= left && arr[j] == target; --j)
+        for (int j = middle - 1; j >= left_number && arr[j] == find_number; --j)
           pair_numbers++;
-        for (int j = mid + 1; j <= right && arr[j] == target; ++j)
+        for (int j = middle + 1; j <= right_number && arr[j] == find_number; ++j)
           pair_numbers++;
           break;
       }
-      else if (arr[mid] < target) {
-        left = mid + 1;
+      else if (arr[middle] < find_number) {
+        left_number = middle + 1;
       }
       else {
-        right = mid - 1;
+        right_number = middle - 1;
       }
     }
   }
